1c08754fa0c391aab0b565996bc3ccd14e91791d
Shahbaaz <mac@macs-MacBook-Pro.local>
2024-07-14 14:24:53 +0500

    first

--- download_models.sh
+++ download_models.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /app/models
-wget -O /app/models/ESRGAN_x4.pth https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth
+wget -O /app/models/SRCNN_x4.pb https://github.com/Saafke/FSRCNN_Tensorflow/raw/master/models/FSRCNN_x4.pb
